6d2741c5277859315a82cd7a2a75c7d9436bfe93
matteoboccardo <91879241+matteoboccardo@users.noreply.github.com>
2021-11-11 12:25:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+    int n=0, precedente=0, successivo=1, risultato =0, i=0;
+    precedente=i;
+    cin>>n;
+    cout<<1<<endl;
+    for (int i = 1; i <=n ; i++) {
+        risultato = precedente + successivo;
+        precedente = successivo, successivo = risultato;
+        if (risultato <= n) {
+            cout << risultato << endl;
+        }
+    }
    return 0;
 }
