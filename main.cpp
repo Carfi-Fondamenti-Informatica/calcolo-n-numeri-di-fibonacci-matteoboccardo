f022ce73c51e565db88f849b9cf4a73ae6a671f4
matteoboccardo <91879241+matteoboccardo@users.noreply.github.com>
2021-11-11 12:30:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     int n=0, precedente=0, successivo=1, risultato =0, i=0;
     precedente=i;
     cin>>n;
-    cout<<1<<endl;
+    cout<<"1"<<endl;
     for (int i = 2; i <=n ; i++) {
         risultato = precedente + successivo;
         precedente = successivo, successivo = risultato;
